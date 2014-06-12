e34a969970cb9a6f4341b68c41436336068fb8e6
JhonathanCatari <joni_4200@hotmail.com>
2014-06-12 10:12:31 -0430

    Proyecto
    Aquí hay un cambio

---
+++ pronombres.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Lección </title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="css/estilo.css" />
+		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel="stylesheet" type="text/css" />
+		<? php require("config.php"); ?>
+</head>
+
+<body>
+	<nav id="nav">
+		<ul>
+			<li><a href="index.html">Inicio</a></li>
+			<li><a href="#banner">Temas</a></li>
+			<li><a href="#main">Instrucciones</a></li>
+			<li><a href="#features">Acerca de</a></li>
+			<li><a href="#">Traductor</a></li>
+
+		</ul>
+	</nav>
+
+	<header><h1>Traduce</h1></header>
+	<section>
+	
+		<article id="contenedor">
+			<h2>Estoy bebiendo</h2>
+			
+				
+      			<?php include "Imprimir()" ?>
+    				<br>
+    				
+    				<br>
+
+ 		 		</p>
+ 		 	</form>	
+
+ 		</article>
+		
+ 	</section>
+	
+	<footer>
+		<section class="contact">
+			<h3>Contactanos</h3>
+			<ul class="icons">
+				<li><a href="#" class="fa fa-twitter solo"><span>Twitter</span></a></li>
+				<li><a href="#" class="fa fa-facebook solo"><span>Facebook</span></a></li>
+				<li><a href="#" class="fa fa-google-plus solo"><span>Google+</span></a></li>
+				<li><a href="#" class="fa fa-pinterest solo"><span>Pinterest</span></a></li>
+			</ul>
+		</section>
+		<div class="copyright">
+			<ul class="menu">
+				<li>&copy; IEB. Todos los Derechos Reservados</li>
+				<li>Ingles Basico Interactivo</li>
+			</ul>
+		</div>
+	</footer>
+</body>
+</html>
